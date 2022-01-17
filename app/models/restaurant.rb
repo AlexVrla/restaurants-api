8f01f8134d34c39e52183b3159c94620f1087320
Alexandre Vrla <alexandre.vrla@gmail.com>
2022-01-17 11:01:41 +0100

    5 endpoints done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 end
